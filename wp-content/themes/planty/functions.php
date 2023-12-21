aed7df0f82361bc3f362e98c5d3b09ad348309f6
Chris79240 <oservice.fr@gmail.com>
2023-12-21 23:13:45 +0100

    formulaires

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,6 +5,9 @@ function theme_enqueue_styles()
 {
     // Ajoute le style CSS du thème parent au thème enfant
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), 
+    filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
 // Ajouter le lien "Admin" dans le menu
@@ -13,8 +16,9 @@ function ajouter_lien_admin_menu( $items, $args ) {
     if ( is_user_logged_in() ) {
         // Ajouter le lien "Admin" à l'emplacement spécifié
         //$items = str_replace( 'id="menu-item-34"', 'id="menu-item-34" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-34"', $items );
-        $items .= '<li><a href="http://localhost/test/planty/wp-admin/">1000Admin</a></li>';
-;
+        $items .= '<li><a href="http://localhost/test/planty/wp-admin/">Admin</a></li>';
+
+
     }
     return $items;
 }
@@ -23,10 +27,10 @@ add_filter( 'wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2 );
 
 /*<li id="menu-item-833" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-833"><a href="http://localhost/test/planty/wp-admin/">Admin</a></li>
 <a href="http://localhost/test/planty/wp-admin/">Admin</a>
-</li>
+</li>*/
 
-/*
-custom_menu_item($items, $args) {
+
+/*custom_menu_item($items, $args) {
     if (is_user_logged_in()) {
         $items .= '<li><a href="#">Admin</a></li>';
         // Récupère les items de menu sous forme de tableau
@@ -42,7 +46,7 @@ custom_menu_item($items, $args) {
         $items = implode('</li>', $items_array) . '</li>';
     }
 
-    return $items;
+  return $items;
 }
-add_filter('wp_nav_menu_items', 'custom_menu_item', 10, 2);
-*/
+add_filter('wp_nav_menu_items', 'custom_menu_item', 10, 2);*/
+
